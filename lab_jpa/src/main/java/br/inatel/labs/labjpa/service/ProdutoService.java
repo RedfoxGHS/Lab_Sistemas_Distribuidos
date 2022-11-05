f8f01fa676b2bb004134dcfc8b52dce72b79817e
GabrielHS <gabrielhenri0012@gmail.com>
2022-11-05 16:05:05 -0300

    add lab_jpa

---
+++ lab_jpa/src/main/java/br/inatel/labs/labjpa/service/ProdutoService.java
@@ -0,0 +1,38 @@
+package br.inatel.labs.labjpa.service;
+
+import br.inatel.labs.labjpa.entity.Produto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Service
+@Transactional
+public class ProdutoService {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Produto salvar(Produto p) {
+        p = em.merge(p);
+        return p;
+    }
+
+    public Produto buscarPeloId(Long id) {
+        Produto p = em.find(Produto.class, id);
+        return p;
+    }
+
+    public List<Produto> listar() {
+        List<Produto> listaProduto = em.createQuery("select p from Produto p", Produto.class)
+                .getResultList();
+        return listaProduto;
+    }
+
+    public void remover(Produto p) {
+        p = em.merge(p);
+        em.remove(p);
+    }
+}
